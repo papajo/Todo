a99cc61cdebc6aa8dc4c9ea353211f48c12e3ddb
papajo <joshipv2@gmail.com>
2018-10-01 12:10:31 -0400

    add chai test to fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -16,4 +16,18 @@ function fizzbuzz (n) {
   return out;
 }
 
-fizzbuzz(15);
+//fizzbuzz(15);
+
+describe('fizzbuzz()', function () {
+  it('should return [1] for n = 1', function () {
+    expect(fizzbuzz(1)).toEqual([1]);
+  });
+
+  it('should work for the example given', function () {
+    expect(fizzbuzz(15)).toEqual([
+      1, 2, "Fizz", 4, "Buzz",
+      "Fizz", 7, 8, "Fizz", "Buzz",
+      11, "Fizz", 13, 14, "FizzBuzz"
+    ]);
+  });
+});
